9dae70b2c1482b9628239b54388092cac57c2dde
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2020-06-03 18:04:26 +0200

    working hard

--- Qt/OvelayIcons/OpenCVLib.py
+++ Qt/OvelayIcons/OpenCVLib.py
@@ -2,12 +2,22 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2019 Stefan Hagmann
 
+import cv2
 import numpy as np
 from PyQt5.Qt import QImage, qRgb
 from PyQt5 import QtGui
+from test import test_imghdr
 
 
 class OpenCVLib(object):
+    """
+    for inspiration have a look at
+    https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_tutorials.html
+    """
+
+    def readPNG(self, img):
+        """ preserver all channels """
+        return cv2.imread(img, cv2.IMREAD_UNCHANGED)
 
     def QImage2MAT(self, qimg):
         """Converts a QImage into an opencv MAT format"""
@@ -31,8 +41,13 @@ class OpenCVLib(object):
     def MAT2QImage(self, im, copy=False):
         """ convert CV MAT to QImage
         see https://gist.github.com/smex/5287589 """
+        """
+        Because OpenCV uses BGR order by default, you should first use
+        cvtColor(src, dst, CV_BGR2RGB)
+        to get an image layout that Qt understands
+        """
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)  # important!
         gray_color_table = [qRgb(i, i, i) for i in range(256)]
-
         if im is None:
             return QImage()
         if im.dtype == np.uint8:
@@ -48,3 +63,11 @@ class OpenCVLib(object):
                 elif im.shape[2] == 4:
                     qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_ARGB32)
                     return qim.copy() if copy else qim
+
+    def resizeTo(self, img, width, height):
+        """ resize the Mat to this width and height in px """
+        h, w = img.shape[:2]
+        fac_w = width / w
+        fac_h = height / h
+        # print("%s x %s" % (fac_w * w, fac_h * h))
+        return cv2.resize(img, (int(fac_w * w), int(fac_h * h)), interpolation=cv2.INTER_CUBIC)
--- Qt/OvelayIcons/OvelayIconsTest.py
+++ Qt/OvelayIcons/OvelayIconsTest.py
@@ -18,17 +18,17 @@ class MAIN_UI(PyQt5.QtWidgets.QMainWindow):
         uifile = self.rootDir.joinpath('main.ui')
         self.ui = uic.loadUi(uifile, self)        # load UI inside QMainWindow
 
-        self.CVTest()
+        self.cv = OpenCVLib()
+        self.CVTest(self.ui.image.pixmap())
 
     def closeEvent(self, event):
         ''' window tries to close '''
         # event.ignore()
         pass
 
-    def CVTest(self):
-        cv = OpenCVLib()
-        Qimg = self.ui.image.pixmap().toImage()
-        img = cv.QImage2MAT(Qimg)
+    def CVTest(self, pixmap):
+        Qimg = pixmap.toImage()
+        img = self.cv.QImage2MAT(Qimg)
         # or load from file
         # image = cv2.imread("mexico.jpg")
         overlay = img.copy()
@@ -37,10 +37,88 @@ class MAIN_UI(PyQt5.QtWidgets.QMainWindow):
         cv2.rectangle(overlay, (420, 205), (595, 385), (0, 0, 255), -1)
         # apply the overlay
         # img, alpha, original, beta, gamma, output
-        alpha = 1
-        cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
+        alpha = 0.5
+        cv2.addWeighted(overlay, alpha, output, 1 - alpha, 1.0, output)
         # write back
-        self.ui.image.setPixmap(cv.MAT2QPixmap(output))
+        self.ui.image.setPixmap(self.cv.MAT2QPixmap(output))
+        # Icon Test
+        self.overlayIcon(self.ui.image.pixmap(), "test/ok.png")
+        
+        
+    def copyROI(self, srcMat, dstMat, x, y):
+        """ using ROI = region of interest to copy region """
+        height, width = srcMat.shape[:2]
+        # left upper corner x1, y1, right bottom corner x2, y2
+        # y1:y2, x1:x2
+        roi = srcMat[0:height, 0:width]
+
+        # make both 3 channels RGB
+        # roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGBA)
+        # dstMat = cv2.cvtColor(dstMat, cv2.COLOR_BGR2RGB)
+
+        #copy roi to dstMat
+        # y1:y2, x1:x2
+        dstMat[y:y + roi.shape[0], x:x + roi.shape[1]] = roi
+
+        return dstMat
+    
+    def overlayIcon(self, pixmap, icon):
+        Qimg = pixmap.toImage()
+        img = self.cv.QImage2MAT(Qimg)
+        output = img.copy()
+        icon = self.cv.readPNG(icon)
+        
+        cv2.imshow('image', self.overlay_transparent(img, icon, 0, 0))
+
+
+
+    def overlayIcon2(self, pixmap, icon):
+        """ place icon onto pixmap """
+        Qimg = pixmap.toImage()
+        img = self.cv.QImage2MAT(Qimg)
+        output = img.copy()
+        icon = self.cv.readPNG(icon)
+
+        cv2.imshow('icon', icon)
+
+        icon = self.cv.resizeTo(icon, 64, 64)
+        
+        output = self.copyROI(icon, output, 10, 10)
+
+     
+
+
+        # write back
+        self.ui.image.setPixmap(self.cv.MAT2QPixmap(output))
+        
+    def overlay_transparent(self, background_img, img_to_overlay_t, x, y, overlay_size=None):
+        """
+        Overlays a transparant PNG onto another image using CV2
+        @param      background_img    The background image
+        @param      img_to_overlay_t  The transparent image to overlay (has alpha channel)
+        @param      x                 x location to place the top-left corner of our overlay
+        @param      y                 y location to place the top-left corner of our overlay
+        @param      overlay_size      The size to scale our overlay to (tuple), no scaling if None
+        @return     Background image with overlay on top
+        """
+        bg_img = background_img.copy()
+        if overlay_size is not None:
+            img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
+        # Extract the alpha mask of the RGBA image, convert to RGB
+        b, g, r, a = cv2.split(img_to_overlay_t)
+        overlay_color = cv2.merge((b, g, r))
+        # Apply some simple filtering to remove edge noise
+        mask = cv2.medianBlur(a, 5)
+        h, w, _ = overlay_color.shape
+        roi = bg_img[y:y + h, x:x + w]
+        # Black-out the area behind in our original ROI
+        img1_bg = cv2.bitwise_and(roi.copy(), roi.copy(), mask=cv2.bitwise_not(mask))
+        # Mask out the logo from the logo image.
+        img2_fg = cv2.bitwise_and(overlay_color, overlay_color, mask=mask)
+        # Update the original image with our new ROI
+        bg_img[y:y + h, x:x + w] = cv2.add(img1_bg, img2_fg)
+
+        return bg_img
 
 
 def main():
