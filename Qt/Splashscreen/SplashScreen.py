89b5dfec97db046217102f05c9d0ed766f21bc4b
SManAT <s.hagmann@bgbaden-frauen.ac.at>
2020-06-12 14:59:26 +0200

    done

--- Qt/Splashscreen/SplashScreen.py
+++ Qt/Splashscreen/SplashScreen.py
@@ -3,8 +3,8 @@
 # Copyright (C) 2019 Stefan Hagmann
 
 from pathlib import Path
-from PyQt5.Qt import QProgressBar, QFont, QColor
-from PyQt5.QtWidgets import QSplashScreen
+from PyQt5.Qt import QProgressBar, QFont, QColor, Qt
+from PyQt5.QtWidgets import QSplashScreen, QLabel
 from PyQt5 import QtGui, QtCore
 from PyQt5.QtGui import QGuiApplication
 from Qt.OvelayIcons.OpenCVLib import OpenCVLib
@@ -23,6 +23,16 @@ class SplashScreen(QSplashScreen):
         # default values
         self.image = self.rootDir.joinpath("img/loading.jpg").as_posix()
         self.version = "3.x"
+        # pixmap.height - ?
+        self.progress_y = 60
+        # Version Font Size
+        self.vsize = 24
+        # pixmap.width - ?
+        self.vx = 16
+        # pixmap.height - ?
+        self.vy = 26
+        # message font size
+        self.msize = 12
 
         self.cv = OpenCVLib()
 
@@ -30,12 +40,15 @@ class SplashScreen(QSplashScreen):
         self.progressBar.setMinimum(0)
         self.progressBar.setValue(0)
         self.progressBar.setMaximum(100)
-        # center on screen
-        margin = 10
-        # x, y, w, h
-        self.progressBar.setGeometry(margin, self.pixmap().height() - 50,
-                                     self.pixmap().width() - 2 * margin, 20)
         self.progressBar.setTextVisible(False)
+        self.setPositionProgressBar()
+
+        self.message = QLabel(self)
+        self.message.setFont(QFont("Arial", self.msize, QFont.Bold))
+        self.message.setStyleSheet("QLabel { color : #FFFFFF; }")
+        self.message.setAlignment(Qt.AlignCenter)
+        self.setPositionMessage()
+        # self.message.hide()
 
         # CSS Styling
         self.setStyleSheet("""
@@ -48,14 +61,35 @@ class SplashScreen(QSplashScreen):
             }
             """)
 
+    def setMessage(self, msg):
+        self.message.setText("%s ..." % (msg))
+        self.message.show()
+
+    def setPositionMessage(self):
+        """ place Message on screen """
+        # where is progress?
+        p = self.progressBar.geometry()
+        self.message.setGeometry(0, p.y() - self.msize,
+                                 self.pixmap().width(), 2 * self.msize)
+        self.message.updateGeometry()
+
+    def setPositionProgressBar(self):
+        """ place ProgressBar on screen """
+        margin = 10
+        # x, y, w, h
+        self.progressBar.setGeometry(margin, self.pixmap().height() - self.progress_y,
+                                     self.pixmap().width() - 2 * margin, 20)
+        self.progressBar.updateGeometry()
+
     def scale(self, pix):
         gold = 0.618
         h = pix.height()
+        w = pix.width()
 
         # max width 68% of screen
         screen = QGuiApplication.screens()[0]
         new_w = screen.geometry().width() * gold
-        new_h = h * gold
+        new_h = h * new_w / w
         # print("%s x %s" %(w, h))
         # python resize
         # return pix.scaled(new_w, new_h, Qt.KeepAspectRatioByExpanding | Qt.SmoothTransformation)
@@ -78,13 +112,17 @@ class SplashScreen(QSplashScreen):
         painter = QtGui.QPainter()
         painter.begin(splash_pix)
 
-        painter.setFont(QFont("Arial", 8, QFont.Bold))
-        painter.setPen(QColor("#000000"))
-        painter.drawText(0, 0, splash_pix.size().width() - 3,
-                         splash_pix.size().height() - 1,
+        painter.setFont(QFont("Arial", self.vsize, QFont.Bold))
+        painter.setPen(QColor("#FFFFFF"))
+        painter.drawText(0, 0, splash_pix.size().width() - self.vx,
+                         splash_pix.size().height() - self.vy,
                          QtCore.Qt.AlignBottom | QtCore.Qt.AlignRight, self.version)
         painter.end()
         self.setPixmap(self.scale(splash_pix))
+        # ----------------------------------------------
+        # other stuff
+        self.setPositionProgressBar()
+        self.setPositionMessage()
 
     def _incProgressbar(self):
         self.progressBar.setValue(self.progressBar.value() + 1)
