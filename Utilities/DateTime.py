fcad9029beee18eb74e710e1f700f8ccd1505500
S.Hag <s.hagmann@bgbaden-frauen.ac.at>
2022-07-27 09:56:15 +0200

    Create DateTime.py

---
+++ Utilities/DateTime.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+
+def getNow():
+  """ get Now """
+  today = datetime.datetime.now()
+  return today.strftime("%Y-%m-%d %H:%M:%S")
