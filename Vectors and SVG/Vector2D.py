9a1251652a68eec60f4438a87f26ba589a10da77
S.Hag <SMan@mailbox.org>
2025-06-11 11:09:57 +0200

    work

--- Vectors and SVG/Vector2D.py
+++ Vectors and SVG/Vector2D.py
@@ -2,10 +2,12 @@
 Basic 2D Vector Module
 """
 
+import math
+
 
 class Vector2D:
-    x = None
-    y = None
+    x = 0
+    y = 0
 
     # Konstruktor
     def __init__(self, x, y):
@@ -27,8 +29,26 @@ class Vector2D:
     def setY(self, y):
         self.y = y
 
+    def length(self):
+        return math.sqrt(self.x * self.x + self.y * self.y)
+        
+    def scalar(self, k):
+        self.x *= k
+        self.y *= k
+
+def createVecFromPoints(A, B):
+        """Create Vector from A > B"""
+        x = B[0] - A[0]
+        y = B[1] - A[1]
+        return Vector2D(x,y)
 
 if __name__ == "__main__":
     # Running some Tests
     a = Vector2D(12, 2)
     print(a)
+    print("Length %s: %s" % (a, a.length()))
+
+    P1 = [4, 5]
+    P2 = [-3, 2]
+    createVecFromPoints(P1, P2)
+    print("P1 > P2: %s > %s = %s" % (P1, P2, a))
